9cddbac565539a4b5d5544349f4b39279180c24e
Pablo Postigo <pablo@worona.org>
2017-03-14 19:56:38 +0100

    draggable list still not working

--- src/dashboard/components/Root/Menu.jsx
+++ src/dashboard/components/Root/Menu.jsx
@@ -4,21 +4,32 @@ import {SortableContainer, arrayMove} from 'react-sortable-hoc';
 import Card from './Card';
 import Button from '../../elements/Button';
 
-const SortableList = SortableContainer(() => (
+const SortableList = SortableContainer(({items}) => (
   <span>
-    <Card url="https://demo.worona.org/wp-cat/cities/architecture/" label="Category 1" />
-    <br />
-    <Card url="https://demo.worona.org/contact" label="Contact" />
-    <br />
-    <Card url="https://demo.worona.org/legal" label="Legal" />
-    <br />
+    {items.map((url, label, index) =>
+                <Card key={`item-${index}`}  url={url} label={label} style={{ marginBottom: '1em' }} />
+            )}
   </span>
 ));
 
 class SortableComponent extends Component {
+  state =
+  {
+    items: [{label: 'Category 1', url: 'https://demo.worona.org/wp-cat/cities/architecture/'},
+    {label: 'Contact', url: 'https://demo.worona.org/contact'},
+    {label: 'Legal', url: 'https://demo.worona.org/legal'},
+    {label: 'Category 1', url: 'https://demo.worona.org/wp-cat/cities/architecture/'}]
+  }
+  onSortEnd = ({oldIndex, newIndex}) => {
+    let {items} = this.state;
+
+    this.setState({
+        items: arrayMove(items, oldIndex, newIndex)
+    });
+  };
   render() {
     return (
-        <SortableList onSortEnd={this.onSortEnd} useDragHandle />
+        <SortableList items={items} onSortEnd={this.onSortEnd} onSortEnd={this.onSortEnd} useDragHandle />
     )
   }
 }
