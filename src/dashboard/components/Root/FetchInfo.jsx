b97ddf99453adfc1d7edfc3b68f3bbc146e35ccb
Luis Herranz <luisherranz@gmail.com>
2017-03-22 16:54:22 +0100

    Refactor notification

--- src/dashboard/components/Root/FetchInfo.jsx
+++ src/dashboard/components/Root/FetchInfo.jsx
@@ -4,64 +4,61 @@ import * as actions from '../../actions';
 import * as selectors from '../../selectors';
 import styles from './style.css';
 
+const Notification = ({ children, color, icon, refresh, loading }) => (
+  <div className={`notification ${color}`}>
+    <div className="level">
+      <div className="level-left">
+        {children}
+      </div>
+      <div className="level-right is-marginless">
+        <button className={`button ${color} ${loading && 'is-loading'}`} onClick={refresh}>
+          {icon &&
+            <span className={`icon ${styles.fetchInfoButtons}`}>
+              <i className="fa fa-refresh" />
+            </span>
+          }
+        </button>
+      </div>
+    </div>
+  </div>
+);
+Notification.propTypes = {
+  children: React.PropTypes.node.isRequired,
+  color: React.PropTypes.string,
+  refresh: React.PropTypes.func,
+  loading: React.PropTypes.bool,
+  icon: React.PropTypes.bool,
+}
+
 const FetchInfo = ({ status, refresh }) => {
   if (status === 'fetching')
     return (
-      <div className="notification is-warning">
-        <div className="level is-mobile">
-          <div className="level-left">
-            Please wait, we are synchronizing your pages and categories.
-          </div>
-          <div className="level-right is-marginless">
-            <button className="button is-loading is-warning" onClick={refresh} />
-          </div>
-        </div>
-      </div>
+      <Notification color="is-warning" refresh={refresh} loading>
+        Please wait, we are synchronizing your pages and categories.
+      </Notification>
     );
   else if (status === 'succeed')
     return (
-      <div className="notification">
-        <div className="level is-mobile">
-          <div className="level-left">
-            Succeed! We have successfully synchronized your pages and categories.
-          </div>
-          <div className="level-right is-marginless">
-            <button className="button is-light" onClick={refresh}>
-              <span className={`icon ${styles.fetchInfoButtons}`}>
-                <i className="fa fa-refresh" />
-              </span>
-            </button>
-          </div>
-        </div>
-      </div>
+      <Notification color="is-light" refresh={refresh} icon>
+        Succeed! We have successfully synchronized your pages and categories.
+      </Notification>
     );
   else if (status === 'error')
     return (
-      <div className="notification is-danger">
-        <div className="level">
-          <div className="level-left">
-            <span>
-              Something went wrong while synchronizing your pages and categories.
-              Please contact with {' '}
-              <a
-                href="https://www.worona.org/get-help"
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{ color: 'white', fontWeight: 'bold' }}
-              >
-                Worona support.
-              </a>
-            </span>
-          </div>
-          <div className="level-right is-marginless">
-            <button className="button is-danger" onClick={refresh}>
-              <span className={`icon ${styles.fetchInfoButtons}`}>
-                <i className="fa fa-refresh" />
-              </span>
-            </button>
-          </div>
-        </div>
-      </div>
+      <Notification color="is-danger" refresh={refresh} icon>
+        <span>
+          Something went wrong while synchronizing your pages and categories.
+          Please contact with {' '}
+          <a
+            href="https://www.worona.org/get-help"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: 'white', fontWeight: 'bold' }}
+          >
+            Worona support.
+          </a>
+        </span>
+      </Notification>
     );
   return null;
 };
