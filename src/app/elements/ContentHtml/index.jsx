ca6a4973688d84be1396a75ae83ea278296e01ac
Luis Herranz <luisherranz@gmail.com>
2017-04-11 11:38:48 +0200

    Add cors if protocol is https

--- src/app/elements/ContentHtml/index.jsx
+++ src/app/elements/ContentHtml/index.jsx
@@ -1,4 +1,4 @@
-/* eslint-disable react/no-danger */
+/* eslint-disable react/no-danger, no-undef */
 import React from 'react';
 import cheerio from 'cheerio';
 import CaptureLinks from '../CaptureLinks';
@@ -7,6 +7,14 @@ import * as libs from '../../libs';
 const ContentHtml = ({ html, linksColor }) => {
   const $ = cheerio.load(html);
   $('a').attr('style', `color: ${libs.darkenColor(linksColor)};`).attr('target', '_blank');
+  $('img').each((i, e) => {
+    const src = $(e).attr('src');
+    const srcset = $(e).attr('srcset');
+    if (src.startsWith('http://') && window.location.protocol === 'https:') {
+      $(e).attr('src', `https://cors.worona.io/${src}`);
+      $(e).attr('srcset', srcset.replace(/http:\/\//g, 'https://cors.worona.io/http://'));
+    }
+  });
   return (
     <CaptureLinks>
       <div
